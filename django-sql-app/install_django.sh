44393061d3f875efeba68dcf0032fbcc8a692164
Meet Bhagdev <mebha@microsoft.com>
2015-04-23 16:26:41 -0700

    das

--- django-sql-app/install_django.sh
+++ django-sql-app/install_django.sh
@@ -11,7 +11,6 @@ sudo easy_install django
 sudo apt-get -y install freetds-dev freetds-bin
 sudo apt-get -y install python-dev python-pip
 sudo pip install pymssql
-
 # install Apache
 sudo apt-get -y install apache2 libapache2-mod-wsgi
 
@@ -19,41 +18,17 @@ sudo apt-get -y install apache2 libapache2-mod-wsgi
 cd /var/www
 sudo django-admin startproject helloworld
 
-echo """from django.http import HttpResponse
-from django.shortcuts import render
-from django.http import HttpRequest
-from django.template import RequestContext
-from datetime import datetime
-import pymssql
-      
-def contact(request):
-    assert isinstance(request, HttpRequest)
-    html = "<html><body>Hello World!</body><html>"
-    return HttpResponse(html)
-def about(request):
-    assert isinstance(request, HttpRequest)
-    html = "<html><body>Hello World!</body><html>"
-    return HttpResponse(html)
-def home2(request):
-    assert isinstance(request, HttpRequest)
-    html = "<html><body>Hello World!</body><html>"
-    return HttpResponse(html)
+# Create a new file named views.py in the /var/www/helloworld/helloworld directory. This will contain the view
+# that renders the "hello world" page
+echo 'from django.http import HttpResponse
 def home(request):
-    html = "<html><body>Hello World!</body><html>"
-    return HttpResponse(html)""" | sudo tee /var/www/helloworld/helloworld/views.py
-
-
-
-echo """from django.conf.urls import patterns, url
-from datetime import datetime
+    html = "<html><body>Hsello World!</body><html>"
+    return HttpResponse(html)' | sudo tee /var/www/helloworld/helloworld/views.py
+# Update urls.py
+echo "from django.conf.urls import patterns, url
 urlpatterns = patterns('',
-    # Examples:
     url(r'^$', 'helloworld.views.home', name='home'),
-    url(r'^home2', 'helloworld.views.home2', name='home2'),
-    url(r'^home', 'helloworld.views.home', name='home'),
-    url(r'^contact$', 'helloworld.views.contact', name='contact'),
-    url(r'^about', 'helloworld.views.about', name='about'),
-)""" | sudo tee /var/www/helloworld/helloworld/urls.py
+)" | sudo tee /var/www/helloworld/helloworld/urls.py
 
 # Setup Apache
 echo "<VirtualHost *:80>
