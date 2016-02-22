08b3a21780642a42c3a6e5ce3b9039e2d9615a95
Meet Bhagdev <mebha@microsoft.com>
2016-02-21 23:51:38 -0800

    Update install_django.sh

--- django-app/install_django.sh
+++ django-app/install_django.sh
@@ -14,7 +14,7 @@ apt-get -y install apache2 libapache2-mod-wsgi
 wget https://github.com/meet-bhagdev/Django/archive/master.zip
 unzip master.zip -d master
 cd master
-cp -r Django-master /var/www
+echo 'password' | sudo -S cp -r Django-master /var/www
 # create a django app
 cd /var/www
 
