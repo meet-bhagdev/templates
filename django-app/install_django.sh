0d723b69d492a4babb702d48b35995bdf4aaccfd
Meet Bhagdev <mebha@microsoft.com>
2016-02-22 00:26:55 -0800

    Update install_django.sh

--- django-app/install_django.sh
+++ django-app/install_django.sh
@@ -14,10 +14,22 @@ apt-get -y install apache2 libapache2-mod-wsgi
 wget https://github.com/meet-bhagdev/Django/archive/master.zip
 unzip master.zip -d master
 cd master
-echo 'password' | sudo -S cp -r Django-master /var/www
+cp -r Django-master /var/www
 # create a django app
 cd /var/www
+django-admin startproject helloworld
 
+# Create a new file named views.py in the /var/www/helloworld/helloworld directory. This will contain the view
+# that renders the "hello world" page
+echo 'from django.http import HttpResponse
+def home(request):
+    html = "<html><body>Hello Worlsad!</body><html>"
+    return HttpResponse(html)' | tee /var/www/helloworld/helloworld/views.py
+# Update urls.py
+echo "from django.conf.urls import patterns, url
+urlpatterns = patterns('',
+    url(r'^$', 'helloworld.views.home', name='home'),
+)" | tee /var/www/helloworld/helloworld/urls.py
 
 cd Django-master
 python manage.py migrate
@@ -28,7 +40,7 @@ echo "<VirtualHost *:80>
 ServerName $1
 </VirtualHost>
 WSGIScriptAlias / /var/www/Django-master/DjangoWebProject1/wsgi.py
-WSGIPythonPath /var/www/Django-master" | tee /etc/apache2/sites-available/helloworld.conf
+WSGIPythonPath /var/www/helloworld" | tee /etc/apache2/sites-available/helloworld.conf
 
 #enable site
 a2ensite helloworld
