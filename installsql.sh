f7192d80604e2f731857cfe38f98f45f29c2d551
Meet Bhagdev <mebha@microsoft.com>
2017-04-04 18:30:57 -0700

    Update installsql.sh

--- installsql.sh
+++ installsql.sh
@@ -1,4 +1,4 @@
-PASSWORD="1234qwerASDF
+PASSWORD="1234qwerASDF"
 
 curl https://packages.microsoft.com/keys/microsoft.asc | sudo apt-key add -
 curl https://packages.microsoft.com/config/ubuntu/16.04/mssql-server.list | sudo tee /etc/apt/sources.list.d/mssql-server.list
