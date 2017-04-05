ba5ce9081eb8d9825d299f8a5d0219d0cb034a69
Meet Bhagdev <mebha@microsoft.com>
2017-04-05 14:32:19 -0700

    Update installsql.sh

--- installsql.sh
+++ installsql.sh
@@ -1,9 +1,7 @@
-PASSWORD="1234qwerASDF"
-
 curl https://packages.microsoft.com/keys/microsoft.asc | sudo apt-key add -
 curl https://packages.microsoft.com/config/ubuntu/16.04/mssql-server.list | sudo tee /etc/apt/sources.list.d/mssql-server.list
 
 apt-get -y update
 apt-get install -y mssql-server
 
-SA_PASSWORD=$PASSWORD /opt/mssql/bin/mssql-conf setup accept-eula
+SA_PASSWORD=$1 /opt/mssql/bin/mssql-conf setup accept-eula
