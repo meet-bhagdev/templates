efc1c78996ecd64524543e72707e07c8767fabd0
Meet Bhagdev <mebha@microsoft.com>
2017-05-17 11:18:57 -0700

    Update installodbcphp.sh

--- installodbcphp.sh
+++ installodbcphp.sh
@@ -1,8 +1,8 @@
-apt-get -y install php7.0 libapache2-mod-php7.0 mcrypt php7.0-mcrypt php-mbstring php-pear php7.0-dev apache2
+apt-get -y install php7.0 libapache2-mod-php7.0 mcrypt php7.0-mcrypt php-mbstring php-pear php7.0-dev apache2 gcc
 curl https://packages.microsoft.com/config/ubuntu/16.04/prod.list > /etc/apt/sources.list.d/mssql-tools.list
 apt-get update
 ACCEPT_EULA=Y apt-get install msodbcsql
-apt-get install unixodbc-dev
+apt-get -y install unixodbc-dev
 pecl install sqlsrv pdo_sqlsrv
 echo "extension= pdo_sqlsrv.so" >> `php --ini | grep "Loaded Configuration" | sed -e "s|.*:\s*||"`
 echo "extension= sqlsrv.so" >> `php --ini | grep "Loaded Configuration" | sed -e "s|.*:\s*||"`
